c07055561176b69dac06b6b5045838eddfbb1884
paavkaa <terezapavelkova1@gmail.com>
2024-07-01 14:53:03 +0200

    Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import 'primereact/resources/themes/lara-dark-purple/theme.css'
-import 'primereact/resources/primereact.min.css'
-import Menu from "./components/Menu.tsx";
+import 'primereact/resources/themes/lara-dark-purple/theme.css';
+import 'primereact/resources/primereact.min.css';
+import Menu from './components/Menu';
+import Content from "./components/Content.tsx";
 
 function App() {
-  return (
-    <div>
-        <Menu />
-    </div>
-  )
+    return (
+        <div>
+            <Menu />
+            <div className="content">
+                <Content />
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,7 @@ import {Sidebar} from "primereact/sidebar";
 import {useState} from "react";
 import { FiHome } from "react-icons/fi";
 import '../css/menu.css';
+import {Link} from "react-router-dom";
 
 export default function Menu() {
     const [visible, setVisible] = useState(false);
@@ -13,7 +14,13 @@ export default function Menu() {
             </button>
 
             <Sidebar visible={visible}  onHide={() => setVisible(false)}>
-                <h2>Simon says</h2>
+                <h4>Simon says</h4>
+
+                <div>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/scores'}>Scores</Link>
+                </div>
+
             </Sidebar>
         </>
     )
